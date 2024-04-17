cf45245c05f65593d041964ff1ec7f7aafff2b6c
amitbyn <amitbyn@gmail.com>
2024-04-16 20:05:42 -0400

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Ensure the base path is correctly set to resolve /src to the right location
+      '/src/': new URL('./src/', import.meta.url).pathname
+    }
+  }
+});
